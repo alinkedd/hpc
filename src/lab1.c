b048f83fec0b9d78fc36cd58f65c588c5016af4f
Alina Listunova <alina.listunova@gmail.com>
2019-03-15 04:43:49 +0200

    refactor: refactor code

--- src/lab1.c
+++ src/lab1.c
@@ -4,31 +4,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const double EPSILON = 1E-8; // Точність обчислення значення
-const int VALUE_TAG = 1; // Теґ показнику ступеня числа E
-const int TERM_NUMBER_TAG = 2; // Теґ номера поточного члену ряду
-const int TERM_TAG = 3; // Теґ значення поточного члену ряду
-const int BREAK_TAG = 4; // Теґ сигналу про завершення обчислень
+const double EPSILON = 1E-8;  // Calculations precision
+const int X_TAG = 1;
+const int N_TAG = 2;
+const int TERM_TAG = 3;
+const int FINISH_TAG = 4;
 
-double factorial(int value) {
-  if (value < 0) {
-    return NAN;
+// Function to calculate factorial of n
+double factorial(int n) {
+  double product = 1.0;
+
+  if (n == 0) { return product; }
+
+  for (int i = 2; i <= n; i++) {
+    product *= i;
   }
-  else if (value == 0) {
-    return 1.;
-  }
-  else {
-    double fact = 1.;
-    for (int i = 2; i <= value; i++) {
-      fact *= i;
-    }
-    return fact;
-  }
+
+  return product;
 }
 
-double calc_series_term(int term_number, double value) {
-  int coef = 2 * (term_number + 1) - 1;
-  return pow(value, coef) / factorial(coef);
+// Function to calculate sh series term - x^(2n-1) / !(2n-1)
+double calc_sh_term(int n, double x) {
+  int odd_n = 2 * (n + 1) - 1; // n starts from 0, so increment by 1
+  double term = pow(x, odd_n) / factorial(odd_n);
+
+  return term;
+}
+
+void master(int np) {
+  int finish = false;
+  int next_n = 0;
+  int n;
+  double x = 0.5; // main variable
+  double series_sum = 0.0;
+  double term;
+
+  for (int i = 1; i < np; i++) {
+    MPI_Send(&x, 1, MPI_DOUBLE, i, X_TAG, MPI_COMM_WORLD);
+  }
+
+  while(!finish) {
+    n = next_n++;
+
+    for (int i = 1; i < np; i++) {
+      MPI_Send(&next_n, 1, MPI_INT, i, N_TAG, MPI_COMM_WORLD);
+      next_n++;
+    }
+
+    term = calc_sh_term(n, x);
+    series_sum += term;
+
+    if (term < EPSILON) {
+      finish = true;
+    } else {
+      for (int i = 1; i < np; i++) {
+        MPI_Recv(&term, 1, MPI_DOUBLE, i, TERM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        series_sum += term;
+
+        if (term < EPSILON) {
+          finish = true;
+          break;
+        }
+      }
+    }
+
+    for (int i = 1; i < np; i++) {
+      MPI_Send(&finish, 1, MPI_INT, i, FINISH_TAG, MPI_COMM_WORLD);
+    }
+  }
+
+  printf("%.15lf\n", series_sum);
+}
+
+void slave() {
+  int finish = false;
+  int n;
+  double x;
+  double term;
+
+  MPI_Recv(&x, 1, MPI_DOUBLE, 0, X_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+  while (!finish) {
+    MPI_Recv(&n, 1, MPI_INT, 0, N_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    term = calc_sh_term(n, x);
+
+    MPI_Send(&term, 1, MPI_DOUBLE, 0, TERM_TAG, MPI_COMM_WORLD);
+    MPI_Recv(&finish, 1, MPI_INT, 0, FINISH_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -40,80 +103,13 @@ int main(int argc, char *argv[]) {
   int np;
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-  double exponent;
-
   if (rank == 0) {
-    exponent = 0.5;
-  }
-
-  if (rank == 0) {
-    for (int i = 1; i < np; i++) {
-      MPI_Send(&exponent, 1, MPI_DOUBLE, i, VALUE_TAG, MPI_COMM_WORLD);
-    }
-  }
-  else {
-    MPI_Recv(&exponent, 1, MPI_DOUBLE, 0, VALUE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  }
-
-  int last_term_number = 0;
-  double sum = .0;
-  for (int step = 0; step < 1000; step++) {
-    int term_number;
-
-    if (rank == 0) {
-      term_number = last_term_number++;
-      int current_term_number = last_term_number;
-
-      for (int i = 1; i < np; i++) {
-        MPI_Send(&current_term_number, 1, MPI_INT, i, TERM_NUMBER_TAG,MPI_COMM_WORLD);
-        current_term_number++;
-      }
-      last_term_number = current_term_number;
-    }
-    else {
-      MPI_Recv(&term_number, 1, MPI_INT, 0, TERM_NUMBER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-
-    double term = calc_series_term(term_number, exponent);
-
-    int need_break = false;
-
-    if (rank == 0) {
-      double current_term = term;
-      sum += current_term;
-
-      if (current_term < EPSILON) {
-        need_break = true;
-      }
-
-      for (int i = 1; i < np; i++) {
-        MPI_Recv(&current_term, 1, MPI_DOUBLE, i, TERM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        sum += current_term;
-
-        if (current_term < EPSILON) {
-          need_break = true;
-          break;
-        }
-      }
-
-      for (int i = 1; i < np; i++) {
-        MPI_Send(&need_break, 1, MPI_INT, i, BREAK_TAG, MPI_COMM_WORLD);
-      }
-    }
-    else {
-      MPI_Send(&term, 1, MPI_DOUBLE, 0, TERM_TAG, MPI_COMM_WORLD);
-      MPI_Recv(&need_break, 1, MPI_INT, 0, BREAK_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-
-    if (need_break) {
-      break;
-    }
-  }
-
-  if (rank == 0) {
-    printf("%.15lf\n", sum);
+    master(np);
+  } else {
+    slave();
   }
 
   MPI_Finalize();
+
   return 0;
 }
