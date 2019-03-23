7b963c65d128075670212c89242515d117e9d2b8
Alina Listunova <alina.listunova@gmail.com>
2019-03-23 16:09:04 +0200

    feat: add average calculations

---
+++ get_average.py
@@ -0,0 +1,22 @@
+import os
+
+def main():
+    input_directory = 'output/'
+    output_directory = 'averages/'
+
+    filenames = os.listdir(input_directory)
+
+    for filename in filenames:
+        input_file = open(input_directory + filename, 'r')
+        lines = input_file.readlines();
+        input_file.close()
+
+        count = len(lines)
+        lines_sum = sum(float(line[:-1]) for line in lines)
+        average = lines_sum / count
+
+        output_file = open(output_directory + filename, 'w+')
+        output_file.write(str(average) + '\n')
+        output_file.close()
+
+main()
